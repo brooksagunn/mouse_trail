04d705af850437956037c631ae272f7ddc4abb22
Brooks Gunn <brooks.a.gunn@gmail.com>
2023-11-24 13:38:15 -0500

    Add functionality that creates additional divs at mouse location for white dot trail

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let body = document.body;
 let mainInfo = document.getElementById("main-info");
-let spec = document.createElement("div");
-spec.className = "spec";
 
 console.log(body);
 
 body.addEventListener("mousemove", (e) => {
   mainInfo.textContent = `X position: ${e.clientX}\n Y position: ${e.clientY}`;
+  let spec = document.createElement("div");
+  spec.className = "spec";
+  spec.style.position = "absolute";
   spec.style.left = `${e.clientX}px`;
   spec.style.top = `${e.clientY}px`;
   body.appendChild(spec);
-  console.log(spec);
 });
